2c12c0d349fd7fae88d611c73d8e4935864a29df
Ivan Savelev <ivan.sav.86@gmail.com>
2016-09-22 14:49:07 +0300

    ук
    у

--- bannerhtml/views/index.php
+++ bannerhtml/views/index.php
@@ -12,7 +12,7 @@
 *
 * DISCLAIMER
 *
-* Do not edit or add to this file if you wish to upgrade PrestaShop to newer
+* Do not edit or add to this fil if you wish to upgrade PrestaShop to newer
 * versions in the future. If you wish to customize PrestaShop for your
 * needs please refer to http://www.prestashop.com for more information.
 *
